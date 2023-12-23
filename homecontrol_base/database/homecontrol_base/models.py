527055e29925703bf2742219b1d19b9331c9d86e
2851999 <joelvdavies@btinternet.com>
2023-12-23 13:27:20 +0000

    Fix issue with adding ACDevices

--- homecontrol_base/database/homecontrol_base/models.py
+++ homecontrol_base/database/homecontrol_base/models.py
@@ -12,7 +12,7 @@ class ACDeviceInfoInDB(Base):
     id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String, unique=True, index=True)
     ip_address = Column(String, unique=True)
-    identifier = Column(Integer)
+    identifier = Column(String)
     key = Column(String)
     token = Column(String)
 
