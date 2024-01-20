77a54f9b397c553bd0ffca7badf84e68d8050295
2851999 <joelvdavies@btinternet.com>
2024-01-20 13:01:27 +0000

    Update packages

--- test.py
+++ test.py
@@ -227,10 +227,10 @@ with create_homecontrol_base_service() as service:
     bridge = service.hue.get_bridge("1e9ffff0-960b-4dd9-8372-a16b6df69d0e")
     with bridge.connect() as conn:
         # print(conn.get_room("e7e6883f-85ae-4d28-8dab-7b783445acad"))
-        # colour = list(
-        #     conn.get_room_state("e7e6883f-85ae-4d28-8dab-7b783445acad").lights.values()
-        # )[1].colour
-        # print(f"rgb({colour.r * 255}, {colour.g * 255}, {colour.b * 255})")
+        colour = list(
+            conn.get_room_state("e7e6883f-85ae-4d28-8dab-7b783445acad").lights.values()
+        )[1].colour
+        print(f"rgb({colour.r * 255}, {colour.g * 255}, {colour.b * 255})")
 
         # print(conn.get_room_state("e7e6883f-85ae-4d28-8dab-7b783445acad"))
         # print(
@@ -241,12 +241,12 @@ with create_homecontrol_base_service() as service:
         #         ),
         #     )
         # )
-        print(
-            conn.set_room_state(
-                "e7e6883f-85ae-4d28-8dab-7b783445acad",
-                HueRoomStateUpdate(scene="7bd7f08f-2eac-4bcb-978c-bb49b13d5f5e"),
-            )
-        )
+        # print(
+        #     conn.set_room_state(
+        #         "e7e6883f-85ae-4d28-8dab-7b783445acad",
+        #         HueRoomStateUpdate(scene="7bd7f08f-2eac-4bcb-978c-bb49b13d5f5e"),
+        #     )
+        # )
 
 
 # async def setup_ac():
